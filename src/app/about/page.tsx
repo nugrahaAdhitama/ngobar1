42086e6c4688ecd67551c9714cd186319a45acf8
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-05-24 22:52:23 +0700

    Add About and Contact pages with metadata and styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,44 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
+
+export const metadata = {
+  title: "About TWD",
+  description: "Learn more about TWD",
+};
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About TWD</h2>
+        <h1 className={styles.title}>
+          We create digital products that help businesses grow.
+        </h1>
+        <p className={styles.desc}>
+          We are a team of designers, developers, and product managers who are
+          passionate about creating digital products that help businesses grow.
+        </p>
+
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>300+</h1>
+            <p>Happy Clients</p>
+          </div>
+          <div className={styles.box}>
+            <h1>150+</h1>
+            <p>Digital Products</p>
+          </div>
+          <div className={styles.box}>
+            <h1>100+</h1>
+            <p>Helpful Applications</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="About TWD" fill className={styles.img} />
+      </div>
+    </div>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
+
+export const metadata = {
+  title: "Contact TWD",
+  description: "Contact TWD",
+};
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="email" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols={10}
+            rows={10}
+            placeholder="Leave your message here..."
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
